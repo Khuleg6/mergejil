1328c204bfa8f77927f007719fc5a62de46c8a46
Rocket-User <example@example.com>
2026-08-25 08:42:47 +0000

    Local Commit: 2026-08-25T08:42:43.587Z - Fixed cross-origin blocked request error by adding `allowedDevOrigins: ['mergejil4874.builtwithrocket.new']` to `next.config.ts`

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['mergejil4874.builtwithrocket.new'],
   turbopack: {},
   webpack(config, { dev }) {
     if (dev) {
